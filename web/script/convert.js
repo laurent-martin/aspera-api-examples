21110b3bd1c85de6f6ccd9fe3ce2ab55e6dc663f
Laurent Martin <laurent.martin.l@gmail.com>
2023-06-30 12:17:08 +0200

    moved to server

--- web/script/convert.js
+++
@@ -1,4 +0,0 @@
-// convert yaml configuration file to javascript
-const yaml = require('js-yaml');
-const fs   = require('fs');
-console.log("config="+JSON.stringify(yaml.load(fs.readFileSync(process.argv[2], 'utf8')))+";");
