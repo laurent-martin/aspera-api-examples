80c0efa3d9956eefa86fb70b490acf9eed04c586
Laurent Martin <laurent.martin.l@gmail.com>
2023-06-30 12:28:24 +0200

    rename to client

--- web/src/client.js
+++ web/src/client.js
@@ -1,5 +1,7 @@
 // sample client web application
 // See reference: https://ibm.github.io/aspera-connect-sdk-js/
+// functions starting with "my_" are used locally
+// functions starting with "client_" are called by the UI
 
 // files selected by user for upload
 var selected_upload_files = []
@@ -11,22 +13,22 @@ const HTTPGW_FORM_ID = 'send-panel'
 const CONNECT_SDK_CDN_LOCATION = '//d3gcli72yxqn2z.cloudfront.net/downloads/connect/latest'
 
 // UI feedback on error
-function app_error(message) {
+function my_error(message) {
     console.error(`ERROR: ${message}`)
     alert(`ERROR: ${message}`)
 }
 
 // @return the provided number with magnitude qualifier
-function app_readableBytes(bytes) {
+function my_readableBytes(bytes) {
     const magnitude = Math.floor(Math.log(bytes) / Math.log(1024))
     const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
     return `${(bytes / Math.pow(1024, magnitude)).toFixed(2) * 1} ${sizes[magnitude]}`
 }
 
 // Called after page full download
-function app_initialize() {
+function client_initialize() {
     if (document.location.protocol === 'file:') {
-        app_error(`ERROR: This page requires use of the nodejs server.`)
+        my_error(`ERROR: This page requires use of the nodejs server.`)
     }
     // initialize values in UI from config file
     document.getElementById('httpgw_url').value = config.httpgw.url
@@ -36,15 +38,15 @@ function app_initialize() {
     document.getElementById('download_file').value = config.server.download_file
     document.getElementById('upload_folder').value = config.server.upload_folder
     // Event listener when user click on UI
-    document.querySelectorAll('input[type=radio]').forEach(item => item.addEventListener('change', () => app_updateUi()))
-    document.querySelectorAll('input[type=checkbox]').forEach(item => item.addEventListener('change', () => app_updateUi()))
-    document.getElementById('use_connect').addEventListener('click', () => { app_updateUi() })
-    document.getElementById('action_download').addEventListener('click', () => { app_updateUi() })
-    app_updateUi()
+    document.querySelectorAll('input[type=radio]').forEach(item => item.addEventListener('change', () => my_updateUi()))
+    document.querySelectorAll('input[type=checkbox]').forEach(item => item.addEventListener('change', () => my_updateUi()))
+    document.getElementById('use_connect').addEventListener('click', () => { my_updateUi() })
+    document.getElementById('action_download').addEventListener('click', () => { my_updateUi() })
+    my_updateUi()
 }
 
 // initializes Aspera Connect: check if extension and client are installed, else ask to install
-function app_initialize_connect() {
+function my_initialize_connect() {
     // object to interact with connect
     var connect_object = new AW4.Connect({
         minVersion: '4.2.0',
@@ -84,7 +86,7 @@ function app_initialize_connect() {
 
 // Generates a transfer spec without calling node API: authorization with bare SSH credentials
 // this is for demo only, usually it would not be the case
-function app_getTransferSpecSSH(params) {
+function my_getTransferSpecSSH(params) {
     // replace ssh, as browser will not parse ssh as scheme
     const serverUrl = new URL(document.getElementById('server_url').value.replace(/^ssh:/g, 'http://'))
     const transferSpec = {
@@ -110,12 +112,12 @@ function app_getTransferSpecSSH(params) {
 // Generate transfer spec for specified transfer operation (upload/download) and files
 // call the app server who will forward to node
 // @return transfer spec with token by calling the local express server
-function app_getTransferSpecFromServer(params) {
+function my_getTransferSpecFromServer(params) {
     console.log(`Transfer requested: ${params.operation}`)
-    const app_server_url = window.location.href
+    const server_url = window.location.href
     return new Promise((resolve) => {
         // get transfer spec from server
-        fetch(`${app_server_url}tspec`, {
+        fetch(`${server_url}tspec`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(params)
@@ -127,7 +129,7 @@ function app_getTransferSpecFromServer(params) {
 
 // start transfer for specified transfer type and files
 // using either connect SDK or HTTP HW SDK
-function app_startTransfer(transferSpec) {
+function my_startTransfer(transferSpec) {
     console.log('With ts=', transferSpec)
     if (document.getElementById('use_connect').checked) {
         this.client.startTransfer(transferSpec)
@@ -137,19 +139,19 @@ function app_startTransfer(transferSpec) {
         //transferSpec.zip_required=true
         if (transferSpec.direction === 'receive') {
             asperaHttpGateway.download(transferSpec).then(response => {
-            }).catch(error => { app_error(`Problem with HTTPGW: ${error.message}`) })
+            }).catch(error => { my_error(`Problem with HTTPGW: ${error.message}`) })
         } else {
             asperaHttpGateway.upload(transferSpec, HTTPGW_FORM_ID)
                 .then(response => { console.log('Upload started', response) })
-                .catch(error => { app_error(`Problem with HTTPGW: ${error.message}`) })
+                .catch(error => { my_error(`Problem with HTTPGW: ${error.message}`) })
         }
     }
 }
 
 // reset file selection for upload to empty file list
-function app_resetSelection() {
+function my_resetSelection() {
     selected_upload_files = []
-    app_updateUi()
+    my_updateUi()
 }
 
 // callback for feedback on transfer (Connect or HTTPGW)
@@ -159,17 +161,17 @@ function handleTransferEvents(transfers) {
     - Id:         ${transfer.uuid},
     - Status:     ${transfer.status},
     - Percent:    ${(transfer.percentage * 100).toFixed(2)}%,
-    - Data Sent:  ${app_readableBytes(transfer.bytes_written)},
-    - Data Total: ${app_readableBytes(transfer.bytes_expected)}`
+    - Data Sent:  ${my_readableBytes(transfer.bytes_written)},
+    - Data Total: ${my_readableBytes(transfer.bytes_expected)}`
         console.log(status)
         document.getElementById('status').innerHTML = status
     })
-    app_updateUi()
+    my_updateUi()
 }
 
 // update dynamic elements in UI
 // initialize selected SDK for transfer: Connect or HTTPGW
-function app_updateUi() {
+function my_updateUi() {
     console.log('update UI')
     document.getElementById('upload_files').innerHTML = selected_upload_files.join(', ')
     if (document.getElementById('use_connect').checked) {
@@ -178,7 +180,7 @@ function app_updateUi() {
         document.getElementById('httpgw_info').style.display = 'none'
         document.getElementById('div_ssh_creds_selector').style.display = 'block'
         if (!this.client) {
-            this.client = app_initialize_connect()
+            this.client = my_initialize_connect()
             // optionally for the sample: follow transfer progress in page
             this.client.addEventListener(AW4.Connect.EVENT.TRANSFER, (eventType, eventInfo) => { handleTransferEvents(eventInfo.transfers) })
         }
@@ -202,7 +204,7 @@ function app_updateUi() {
                 })
                 .catch(error => {
                     document.getElementById('httpgw_version').innerHTML = `HTTP GW ${error.message}`
-                    app_error(`Problem with HTTPGW: ${error.message}`)
+                    my_error(`Problem with HTTPGW: ${error.message}`)
                 })
         }
     }
@@ -220,49 +222,49 @@ function app_updateUi() {
     }
 }
 
-function app_start_transfer() {
+function client_start_transfer() {
     var params = null
     if (document.getElementById('action_download').checked) {
         params = { operation: 'download', sources: [document.getElementById('download_file').value] }
     } else {
         params = { operation: 'upload', sources: selected_upload_files, destination: document.getElementById('upload_folder').value }
-        app_resetSelection()
+        my_resetSelection()
     }
     var download_type = document.querySelector("input[type='radio'][name=transfer_auth]:checked").value
     if (download_type === "ssh_creds") {
-        app_startTransfer(app_getTransferSpecSSH(params))
+        my_startTransfer(my_getTransferSpecSSH(params))
     } else {
         // this calls the nodejs server which calls the node api
-        app_getTransferSpecFromServer(params)
+        my_getTransferSpecFromServer(params)
             .then((transferSpec) => {
                 // for basic token, we normally do not need to call the node api, but that is safer to get actual transfer addresses and a pre-filled transfer spec
                 if (download_type === "basic_token") { transferSpec.token = 'Basic ' + btoa(document.getElementById('node_user').value + ':' + document.getElementById('node_pass').value) }
                 // for HTTPGW or Connect SDK to use Aspera SSH keys for token, specify this in transfer spec
                 transferSpec.authentication = 'token'
-                app_startTransfer(transferSpec)
-            }).catch((message) => { app_error(message) })
+                my_startTransfer(transferSpec)
+            }).catch((message) => { my_error(message) })
     }
 }
 
 
 // callback after files are selected
-function app_storeFileNames(selection) {
+function my_storeFileNames(selection) {
     for (const file of selection.dataTransfer.files) {
         selected_upload_files.push(file.name)
     }
     console.log('Files picked', selected_upload_files)
-    app_updateUi()
+    my_updateUi()
 }
 
 // Button: Select files for upload
-function app_pick_files() {
+function client_pick_files() {
     // for the sample: a new select deletes already selected files
-    app_resetSelection()
+    my_resetSelection()
     if (document.getElementById('use_connect').checked) {
         this.client.showSelectFileDialogPromise({ allowMultipleSelection: false })
-            .then((selection) => { app_storeFileNames(selection) })
+            .then((selection) => { my_storeFileNames(selection) })
             .catch(() => { console.error('Unable to select files') })
     } else {
-        asperaHttpGateway.getFilesForUpload((selection) => { app_storeFileNames(selection) }, HTTPGW_FORM_ID)
+        asperaHttpGateway.getFilesForUpload((selection) => { my_storeFileNames(selection) }, HTTPGW_FORM_ID)
     }
 }
