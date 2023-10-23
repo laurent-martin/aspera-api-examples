a568b820f64a71f75978770184772849f1c12997
Laurent Martin <laurent.martin.l@gmail.com>
2023-10-23 14:27:29 +0200

    simplification, add shutdown

--- python/src/server.py
+++ python/src/server.py
@@ -3,54 +3,52 @@
 # transfer files with Aspera HSTS using SSH authentication
 import test_environment
 import logging
-import sys
 import os
 from urllib.parse import urlparse
 
-assert 'server' in test_environment.CONFIG, 'server config is missing'
+assert "server" in test_environment.CONFIG, "server config is missing"
 
-server_url = urlparse(test_environment.CONFIG['server']['url'])
-assert server_url.scheme == 'ssh', 'expecting SSH scheme for server URL'
+# where transferred files will be stored
+tmp_folder = os.environ["CONFIG_TMPDIR"]
+
+server_url = urlparse(test_environment.CONFIG["server"]["url"])
+assert server_url.scheme == "ssh", "expecting SSH scheme for server URL"
 
 remote_host = server_url.hostname
 remote_port = server_url.port
-remote_user = test_environment.CONFIG['server']['user']
-remote_pass = test_environment.CONFIG['server']['pass']
+remote_user = test_environment.CONFIG["server"]["user"]
+remote_pass = test_environment.CONFIG["server"]["pass"]
 
 # Example 1: download
 # Instead of using the soon deprecated FaspManager1 Python lib, let's use the transfer spec
 # direction is relative to us, client, i.e. receive = download
-logging.debug('======Test 1: download')
+logging.debug("======Test 1: download")
 t_spec_download = {
-    'remote_host': remote_host,
-    'ssh_port': remote_port,
-    'remote_user': remote_user,
-    'remote_password': remote_pass,
-    'direction': 'receive',
-    'destination_root': test_environment.tmp_folder,
-    'paths': [
-        {'source': '/aspera-test-dir-tiny/200KB.1'}
-    ]
+    "remote_host": remote_host,
+    "ssh_port": remote_port,
+    "remote_user": remote_user,
+    "remote_password": remote_pass,
+    "direction": "receive",
+    "destination_root": tmp_folder,
+    "paths": [{"source": "/aspera-test-dir-tiny/200KB.1"}],
 }
 test_environment.start_transfer_and_wait(t_spec_download)
 
 # location of downloaded file
-local_file = os.path.join(test_environment.tmp_folder, '200KB.1')
+local_file = os.path.join(tmp_folder, "200KB.1")
 
 # Example 2: upload: single file upload to existing folder.
-logging.debug('======Test 2: upload file')
+logging.debug("======Test 2: upload file")
 t_spec_upload = {
-    'remote_host': remote_host,
-    'ssh_port': remote_port,
-    'remote_user': remote_user,
-    'remote_password': remote_pass,
-    'direction': 'send',
-    'destination_root': '/Upload',
+    "remote_host": remote_host,
+    "ssh_port": remote_port,
+    "remote_user": remote_user,
+    "remote_password": remote_pass,
+    "direction": "send",
+    "destination_root": "/Upload",
     # 'create_dir':True, # destination root is folder, else it assumes (one source) it is dest file name
-    'paths': [
-        {'source': local_file}
-    ],
-    'tags': {'mysample_tag': 'hello'}
+    "paths": [{"source": local_file}],
+    "tags": {"mysample_tag": "hello"},
 }
 test_environment.start_transfer_and_wait(t_spec_upload)
 # check file is uploaded by connecting to: http://demo.asperasoft.com/aspera/user/ with same creds
@@ -59,16 +57,14 @@ test_environment.start_transfer_and_wait(t_spec_upload)
 # if there is only one source file and destination does not exist, then "FASP" assumes it is destination filename
 # but if destination is a folder, it will send same source filename into folder
 # so enforce folder creation, to be sure of what happens
-logging.debug('======Test 3: upload file to new folder')
-t_spec_upload['destination_root'] = '/Upload/new_folder'
-t_spec_upload['create_dir'] = True
+logging.debug("======Test 3: upload file to new folder")
+t_spec_upload["destination_root"] = "/Upload/new_folder"
+t_spec_upload["create_dir"] = True
 test_environment.start_transfer_and_wait(t_spec_upload)
 
 # Example 4: upload: send to sub folder, but using file pairs
-logging.debug('======Test 4: upload file and rename')
-t_spec_upload['destination_root'] = '/Upload'
-del t_spec_upload['create_dir']
-t_spec_upload['paths'] = [
-    {'source': local_file, 'destination': 'xxx/newfilename.ext'}
-]
+logging.debug("======Test 4: upload file and rename")
+t_spec_upload["destination_root"] = "/Upload"
+del t_spec_upload["create_dir"]
+t_spec_upload["paths"] = [{"source": local_file, "destination": "xxx/newfilename.ext"}]
 test_environment.start_transfer_and_wait(t_spec_upload)
--- python/src/test_environment.py
+++ python/src/test_environment.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python3
 # laurent.martin.aspera@fr.ibm.com
+# Common library for sample scripts
 # Helper methods to get API environment according to config file
-import sys
+# Simplified function to start transfer and wait for it to finish
 import os
 import yaml
 import logging
+import grpc
+import time
+import json
+import sys
+import subprocess
 from http.client import HTTPConnection
+from urllib.parse import urlparse
+
+
+assert (
+    "CONFIG_TRSDK_DIR_GENERIC" in os.environ
+), "env var CONFIG_TRSDK_DIR_GENERIC is missing"
+
+# tell where to find gRPC stubs: transfer_pb2 and transfer_pb2_grpc
+sys.path.insert(
+    1, os.path.join(os.environ["CONFIG_TRSDK_DIR_GENERIC"], "connectors", "python")
+)
+
+import transfer_pb2 as transfer_manager
+import transfer_pb2_grpc as transfer_manager_grpc
+
+assert (
+    "CONFIG_TRSDK_DIR_ARCH" in os.environ
+), "env var CONFIG_TRSDK_DIR_ARCH is missing. To load environment execute: . ../../config.env"
+
+# use "ascp" in PATH, add the one from SDK
+os.environ["PATH"] += os.environ["CONFIG_TRSDK_DIR_ARCH"]
+
+assert (
+    "CONFIG_TMPDIR" in os.environ
+), "env var CONFIG_TMPDIR is missing. To load environment execute: . ../../config.env"
+
 
 # If the sample script is started individually, set env vars by executing: . ../../config.env
 assert (
     "CONFIG_YAML" in os.environ
 ), "env var CONFIG_YAML is missing. To load environment execute: . ../../config.env"
-assert (
-    "CONFIG_TMPDIR" in os.environ
-), "env var CONFIG_TMPDIR is missing. To load environment execute: . ../../config.env"
-assert (
-    "CONFIG_TRSDK_DIR_ARCH" in os.environ
-), "env var CONFIG_TRSDK_DIR_ARCH is missing. To load environment execute: . ../../config.env"
-assert (
-    "CONFIG_TRSDK_DIR_GENERIC" in os.environ
-), "env var CONFIG_TRSDK_DIR_GENERIC is missing"
 
-# depending on flag, use new SDK, or old faspmanager
-sys.path.insert(
-    1, os.path.join(os.environ["CONFIG_TRSDK_DIR_GENERIC"], "connectors", "python")
-)
-import helper_aspera_transfer_sdk
-
-# use "ascp" in PATH, add the one from sdk
-os.environ["PATH"] += os.environ["CONFIG_TRSDK_DIR_ARCH"]
-
+# configuration from configuration file
+CONFIG = yaml.load(open(os.environ["CONFIG_YAML"]), Loader=yaml.FullLoader)
 
 # set logger for debugging
 logging.basicConfig()
@@ -41,19 +57,125 @@ requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-# where transferred files will be stored
-tmp_folder = os.environ["CONFIG_TMPDIR"]
+transfer_daemon = None
 
-# configuration from configuration file
-CONFIG = yaml.load(open(os.environ["CONFIG_YAML"]), Loader=yaml.FullLoader)
+TRANSFERD_EXECUTABLE = "asperatransferd"
 
-helper_aspera_transfer_sdk.set_grpc_url(CONFIG["misc"]["trsdk_url"])
+
+def start_daemon(sdk_grpc_url):
+    global transfer_daemon
+    assert isinstance(sdk_grpc_url, str), "call set_grpc_url to set grpc url"
+    # avoid message: Other threads are currently calling into gRPC, skipping fork() handlers
+    os.environ["GRPC_ENABLE_FORK_SUPPORT"] = "false"
+    # create a connection to the transfer manager daemon
+    grpc_url = urlparse(sdk_grpc_url)
+    channel = grpc.insecure_channel(grpc_url.hostname + ":" + str(grpc_url.port))
+    sdk_server = None
+    # try to start daemon a few times if needed
+    for i in range(0, 2):
+        try:
+            print(f"Connecting to {TRANSFERD_EXECUTABLE} using gRPC...")
+            grpc.channel_ready_future(channel).result(timeout=3)
+            print("SUCCESS: connected")
+            # channel is ok, let's get the stub
+            sdk_server = transfer_manager_grpc.TransferServiceStub(channel)
+        except grpc.FutureTimeoutError:
+            print("FAILED: trying to start daemon")
+            # else prepare config and start
+            bin_folder = os.environ["CONFIG_TRSDK_DIR_ARCH"]
+            config = {
+                "address": grpc_url.hostname,
+                "port": grpc_url.port,
+                "fasp_runtime": {
+                    "use_embedded": False,
+                    "user_defined": {
+                        "bin": bin_folder,
+                        "etc": os.environ["CONFIG_TRSDK_DIR_GENERIC"],
+                    },
+                },
+            }
+            tmp_file_base = os.path.join(os.environ["CONFIG_TMPDIR"], "daemon")
+            conf_file = tmp_file_base + ".conf"
+            with open(conf_file, "w") as the_file:
+                the_file.write(json.dumps(config))
+            command = [
+                os.path.join(bin_folder, TRANSFERD_EXECUTABLE),
+                "--config",
+                conf_file,
+            ]
+            out_file = tmp_file_base + ".out"
+            err_file = tmp_file_base + ".err"
+            time.sleep(1)
+            print("Starting: " + " ".join(command))
+            print("stderr: " + err_file)
+            print("stdout: " + out_file)
+            transfer_daemon = subprocess.Popen(
+                " ".join(command),
+                shell=True,
+                stdout=open(out_file, "w"),
+                stderr=open(err_file, "w"),
+            )
+            # give time for startup
+            time.sleep(1)
+        if sdk_server is not None:
+            break
+    if sdk_server is None:
+        print(
+            "ERROR: daemon not started or cannot be started. Check the logs: daemon.err and daemon.out (see paths above)."
+        )
+        exit(1)
+    return sdk_server
+
+
+def start_transfer(sdk_server, transfer_spec):
+    # create a transfer request
+    transfer_request = transfer_manager.TransferRequest(
+        transferType=transfer_manager.FILE_REGULAR,
+        config=transfer_manager.TransferConfig(),
+        transferSpec=json.dumps(transfer_spec),
+    )
+    # send start transfer request to transfer manager daemon
+    transfer_response = sdk_server.StartTransfer(transfer_request)
+    if 4 == transfer_response.status:
+        print("ERROR: {0}".format(transfer_response.error.description))
+        exit(1)
+    return transfer_response.transferId
+
+
+def wait_transfer(sdk_server, transfer_id):
+    print("transfer started with id {0}".format(transfer_id))
+    # monitor transfer status
+    for transfer_info in sdk_server.MonitorTransfers(
+        transfer_manager.RegistrationRequest(
+            filters=[transfer_manager.RegistrationFilter(transferId=[transfer_id])]
+        )
+    ):
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>\ntransfer info {0}".format(transfer_info))
+        # check transfer status in response, and exit if it's done
+        status = transfer_info.status
+        # exit on first success or failure
+        if status == transfer_manager.COMPLETED:
+            print("finished {0}".format(status))
+            break
+        if status == transfer_manager.FAILED:
+            raise Exception(transfer_info.message)
+
+
+def shutdown():
+    global transfer_daemon
+    if transfer_daemon is not None:
+        transfer_daemon.terminate()
+        transfer_daemon = None
+        print("transfer daemon terminated")
+    else:
+        print("transfer daemon already terminated")
 
 
 def start_transfer_and_wait(t_spec):
     # TODO: remove when transfer sdk bug fixed
     t_spec["http_fallback"] = False
     logging.debug(t_spec)
-    sdk_server = helper_aspera_transfer_sdk.start_daemon()
-    t_id = helper_aspera_transfer_sdk.start_transfer(sdk_server, t_spec)
-    helper_aspera_transfer_sdk.wait_transfer(sdk_server, t_id)
+    sdk_server = start_daemon(CONFIG["misc"]["trsdk_url"])
+    t_id = start_transfer(sdk_server, t_spec)
+    wait_transfer(sdk_server, t_id)
+    shutdown()
