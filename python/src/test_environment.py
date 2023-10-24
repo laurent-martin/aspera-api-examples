26d76fef7e0be8a122f2bdecb175827fc955c67e
Laurent Martin <laurent.martin.l@gmail.com>
2023-10-24 19:28:57 +0200

    doc

--- python/src/test_environment.py
+++ python/src/test_environment.py
@@ -18,45 +18,51 @@ from urllib.parse import urlparse
 
 # get project root folder
 top_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+# config file with sub-paths in project's root folder
 PATHS_FILE = "config/paths.yaml"
 
-# get project's relative paths
+# read project's relative paths config file
 PATHS = yaml.load(open(os.path.join(top_folder, PATHS_FILE)), Loader=yaml.FullLoader)
+
+# Error hint to help user to fix the issue
+ERROR_HINT = f"\nPlease check: SDK installed in {PATHS['sdk_root']}, configuration file: {PATHS['mainconfig']}"
 
 
 def get_path(name):
     """Get con figuration sub-path in project's root folder"""
     item_path = os.path.join(top_folder, *PATHS[name].split("/"))
-    assert os.path.exists(
-        item_path
-    ), f"ERROR: {item_path} not found, check configuration file: {PATHS_FILE}"
+    assert os.path.exists(item_path), f"ERROR: {item_path} not found.{ERROR_HINT}"
     return item_path
 
 
-# configuration from configuration file
+# Read configuration from configuration file
 CONFIG = yaml.load(open(get_path("mainconfig")), Loader=yaml.FullLoader)
+
+# location of gRPC stubs
 python_stub_folder = os.path.join(get_path("trsdk_noarch"), "connectors", "python")
 
 assert os.path.exists(
     python_stub_folder
-), f"ERROR: python stubs not found in: {python_stub_folder}\nPlease check that SDK is installed."
-
-# avoid incompatibility of version
-os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+), f"ERROR: python stubs not found in: {python_stub_folder}.{ERROR_HINT}"
 
 # tell where to find gRPC stubs: transfer_pb2 and transfer_pb2_grpc
 sys.path.insert(1, python_stub_folder)
 
+# before stub import: protobuf: avoid incompatibility of version, use pure python implementation
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+# import gRPC stubs (Transfer SDK API)
 import transfer_pb2 as transfer_manager
 import transfer_pb2_grpc as transfer_manager_grpc
 
+# folder with executables
 arch_folder = os.path.join(get_path("sdk_root"), CONFIG["misc"]["system_type"])
 assert os.path.exists(
     arch_folder
-), f"ERROR: SDK not found in: {arch_folder}\nPlease check check configuration file."
+), f"ERROR: SDK not found in: {arch_folder}.{ERROR_HINT}"
 # use "ascp" in PATH, add the one from SDK
 os.environ["PATH"] += arch_folder
-
 
 # set logger for debugging
 logging.basicConfig()
@@ -68,7 +74,7 @@ requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-# global vars
+# Global vars
 transfer_daemon_process = None
 sdk_client = None
 shutdown_after_transfer = True
@@ -80,6 +86,7 @@ TRANSFERD_EXECUTABLE = "asperatransferd"
 
 
 def start_daemon(sdk_grpc_url):
+    """Start transfer manager daemon if not already running and return gRPC client"""
     global transfer_daemon_process
     global sdk_client
     # avoid message: "Other threads are currently calling into gRPC, skipping fork() handlers"
@@ -147,6 +154,7 @@ def start_daemon(sdk_grpc_url):
 
 
 def start_transfer(transfer_spec):
+    """Start a transfer and return transfer id"""
     global sdk_client
     # create a transfer request
     transfer_request = transfer_manager.TransferRequest(
@@ -163,6 +171,7 @@ def start_transfer(transfer_spec):
 
 
 def wait_transfer(transfer_id):
+    """Wait for transfer to finish"""
     global sdk_client
     print("transfer started with id {0}".format(transfer_id))
     # monitor transfer status
@@ -183,6 +192,7 @@ def wait_transfer(transfer_id):
 
 
 def shutdown():
+    """Shutdown transfer manager daemon, if needed"""
     global transfer_daemon_process
     if transfer_daemon_process is not None:
         # transfer_daemon_process.send_signal(signal.CTRL_C_EVENT)
@@ -196,6 +206,7 @@ def shutdown():
 
 
 def start_transfer_and_wait(t_spec):
+    """One-call simplified procedure to start daemon, transfer and wait for it to finish"""
     global sdk_client
     global shutdown_after_transfer
     # TODO: remove when transfer sdk bug fixed
