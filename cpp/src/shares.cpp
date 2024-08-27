fbd45b95447ce88277d32eff74686bd77d167fdc
Laurent Martin <laurent.martin.l@gmail.com>
2024-08-27 23:45:07 +0200

    cleanup

--- cpp/src/shares.cpp
+++ cpp/src/shares.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
                 {"transfer_requests", json::array{json::object{{"transfer_request", json::object{{"paths", json::array{json::object{{"destination", destination_folder_in_shares}}}}}}}}}};
             json::object response_data = shares_api.post("files/upload_setup", transfer_setup_request);
             json::object transfer_specification = response_data["transfer_specs"].as_array()[0].as_object()["transfer_spec"].as_object();
-            test_env.add_files_to_ts(transfer_specification["paths"].as_array(), false);
+            test_env.add_files_to_ts(transfer_specification["paths"].as_array());
             test_env.start_transfer_and_wait(transfer_specification);
         }
         {
@@ -31,7 +31,6 @@ int main(int argc, char* argv[]) {
             json::object response_data = shares_api.post("files/download_setup", transfer_setup_request);
             json::object transfer_specification = response_data["transfer_specs"].as_array()[0].as_object()["transfer_spec"].as_object();
             transfer_specification["destination_root"] = std::filesystem::temp_directory_path().string();
-            // test_env.add_files_to_ts(transfer_specification["paths"].as_array(), true);
             test_env.start_transfer_and_wait(transfer_specification);
         }
         return 0;
