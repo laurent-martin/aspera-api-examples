c373795c3cfddfe6ec9efcfb33d0ca3e8e838360
Laurent Martin <laurent.martin.l@gmail.com>
2024-08-28 02:21:01 +0200

    cleanup

--- cpp/src/node_v2.cpp
+++ cpp/src/node_v2.cpp
@@ -4,19 +4,22 @@
 int main(const int argc, const char* const argv[]) {
     try {
         utils::TestEnvironment test_env(argc, argv);
+        test_env.connect();
         const std::string node_api_url = test_env.conf_str({"node", "url"});
-        json::object transfer_spec_v2 = json::object{
+        const std::string header_authorization = utils::Rest::basic_auth_header(test_env.conf_str({"node", "user"}), test_env.conf_str({"node", "pass"}));
+        json::object transfer_spec_v2 = {
             {"title", "send using Node API and ts v2"},
-            {"session_initiation", json::object{
-                                       {"node_api",
-                                        {{"url", node_api_url},
-                                         {
-                                             "headers",
-                                             json::array{{{"key", "Authorization"},
-                                                          {"value", utils::Rest::basic_auth_header(test_env.conf_str({"node", "user"}), test_env.conf_str({"node", "pass"}))}}},
-                                         }}}}},
+            {"session_initiation", {              //
+                                    {"node_api", {//
+                                                  {"url", node_api_url},
+                                                  {"headers", json::array{
+                                                                  {{"key", "Authorization"},         //
+                                                                   {"value", header_authorization}}  //
+                                                              }}}}}},
             {"direction", "send"},
-            {"assets", json::object{{"destination_root", test_env.conf_str({"node", "folder_upload"})}, {"paths", json::array()}}}};
+            {"assets", {//
+                        {"destination_root", test_env.conf_str({"node", "folder_upload"})},
+                        {"paths", json::array()}}}};
         test_env.add_files_to_ts(transfer_spec_v2["assets"].as_object()["paths"].as_array(), false);
         test_env.start_transfer_and_wait(transfer_spec_v2);
         return 0;
