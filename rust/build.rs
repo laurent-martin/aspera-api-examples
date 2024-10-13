fa070da185b1f2ea2b0a8fefd9501a444ef66769
Laurent Martin <laurent.martin.l@gmail.com>
2024-10-13 11:25:53 +0200

    comment

--- rust/build.rs
+++ rust/build.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+/// compilation of the proto file
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get the proto file path from the environment variable
     let proto_path = env::var("SDK_FILE_PROTO").expect("SDK_FILE_PROTO environment variable must be set");
