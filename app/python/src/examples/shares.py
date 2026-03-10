55d9fac8cd674d09ba1bba7d0e757c8d489d3d48
Laurent Martin <laurent.martin.l@gmail.com>
2026-03-10 18:27:35 +0100

    check shares error code
    Signed-off-by: Laurent Martin <laurent.martin.l@gmail.com>

--- app/python/src/examples/shares.py
+++ app/python/src/examples/shares.py
@@ -11,16 +11,23 @@ transfer_client = utils.transfer_client.TransferClient(config).startup()
 
 try:
     shares_api = utils.rest.Rest(f"{config.param('shares', 'url')}/node_api")
-    shares_api.setAuthBasic(config.param('shares', 'username'), config.param('shares', 'password'))
+    shares_api.setAuthBasic(
+        config.param('shares', 'username'),
+        config.param('shares', 'password'))
     shares_api.setVerify(config.param('shares', 'verify', True))
 
     # call Node API with a single transfer request to get one transfer spec with Aspera token
     logging.info('Generating transfer spec')
     response_data = shares_api.create('files/upload_setup', {
         'transfer_requests': [
-            {'transfer_request': {'paths': [{'destination': config.param('shares', 'folder_upload')}]}}
+            {'transfer_request': {
+                'paths': [{'destination': config.param('shares', 'folder_upload')}]
+            }}
         ]
     })
+
+    if 'error' in response_data:
+        raise Exception(response_data['error']['user_message'])
 
     # extract the single transfer spec (we sent a single transfer request)
     t_spec = response_data['transfer_specs'][0]['transfer_spec']
